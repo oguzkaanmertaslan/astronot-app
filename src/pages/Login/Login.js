ebe226454eb4d0e89060bb8de4d04f1c11450002
Oğuz Kaan Mertaslan <kmertaslan66@gmail.com>
2022-05-21 15:54:58 +0300

    login page written

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,13 +4,22 @@ import "./style.css";
 const Login = () => {
   return (
     <div className="login-area">
+      <h1 className="login-header">Login or Register</h1>
       <div className="input-area">
-        <label className="input-label">User Name</label>
-        <input></input>
-        <label className="input-label">Password</label>
-        <input></input>
+        <div className="username-area">
+          <label className="input-label">User Name</label>
+          <input className="input-field" type="text" />
+        </div>
+        <div className="password-area">
+          <label className="input-label">Password</label>
+          <input className="input-field" type="password" />
+        </div>
       </div>
       <img className="login-img" src={image} alt="bg" />
+      <div className="login-btn-area">
+        <button className="btn">Login</button>
+        <button className="btn">Register</button>
+      </div>
     </div>
   );
 };
