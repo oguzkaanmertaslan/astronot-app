0ad4a683c1e22837785b881d8d363300739b88b6
Oğuz Kaan Mertaslan <kmertaslan66@gmail.com>
2022-05-24 18:38:08 +0300

    register page design written

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,15 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import "./style.css"
 const Register = () => {
-    const [options, setOptions] = useState(["Mars", "Jupıter", "Moon"])
+    const options = ["Mars", "Jupiter", "Moon"]
     return (
         <div className='register-page'>
             <h1 className='register-header'>
                 Register
             </h1>
-            <div className='register-area'>
-                <div className='planet-select-area'>
-                    <label className='planet-label'>Planet</label>
+            <div>
+                <div>
+                    <label className='planet-label'>
+                        Username:
+                    </label>
+                    <input className='reg-input' type="text" />
+                </div>
+                <div>
+                    <label className='planet-label'>
+                        Password:
+                    </label>
+                    <input className='reg-input' type="text" />
+                    <label className='planet-label'>Select Planet</label>
                     <select className='to-select'>
                         {options.map((option, index) => {
                             return <option key={index}>{option}</option>
