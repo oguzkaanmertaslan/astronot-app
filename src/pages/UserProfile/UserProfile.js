fd46aaf3b31a7a76ec832411a4733bcec6c21eb5
Oğuz Kaan Mertaslan <kmertaslan66@gmail.com>
2022-05-30 19:50:44 +0300

    login user set to localstorage and get userprofile page

--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -1,38 +1,52 @@
-import React, {useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { getUser } from '../../components/services/services'
 import "./style.css"
 
 const UserProfile = () => {
-  const [user, setUser] = useState([])
+  const [tableUser, setTableUser] = useState([])
+  const user = (localStorage.getItem('user')) ? JSON.parse(localStorage.getItem('user')) : null
   const tableGetUSer = async () => {
-      const response = await getUser();
-      setUser(response);
+    const response = await getUser();
+    setTableUser(response);
   };
   useEffect(() => {
-      tableGetUSer();
+    tableGetUSer();
   }, []);
-  
+  console.log(user);
   return (
+    <>
+    {
+      user.map(user => (
+        <div className="user-container">
+          <p>{user.fullname}</p>
+          <p>{user.planet}</p>
+          <p>{user.date}</p>
+</div>
+      ))
+    }
       <div className='table-area'>
-                <table className='table'>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Planet</th>
-                            <th>Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {user.map(user => (
-                            <tr>
-                                <td>{user.fullname}</td>
-                                <td>{user.planet}</td>
-                                <td>{user.date}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                    </table>
-            </div>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Planet</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableUser.map(user => (
+              <tr key={user.id}>
+                <td>{user.fullname}</td>
+                <td>{user.planet}</td>
+                <td>{user.date}</td>
+              </tr>
+            ))}
+
+          </tbody>
+
+        </table>
+      </div>
+    </>
   )
 }
 
